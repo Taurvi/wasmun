c29253a6e282cc12d82804adab0c6bed1856e5ef
Taurvi <srimbakusumo@gmail.com>
2015-08-09 01:33:19 -0700

    Forgot to add logo.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,3 @@
-var api_education_key = '87870540729d04d5dc89a93b8063f9db';
-var api_education_address = 'http://api.education.com/service/service.php?f=schoolSearch&key=' + api_education_key + '&sn=sf&v=4&zip=98155&resf=json';
-
-var newData;
-$.ajax({
-    url: api_education_address,
-    dataType: 'jsonp',
-    success: function(data) {
-        newData = data
-    }
-});
-
 //  Create Angular App
 var ngApp = angular.module('ngApp', ['ngRoute']);
 
