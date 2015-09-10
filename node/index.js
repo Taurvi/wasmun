77872afb0a99cf574c0f99e20dc874f76ed76c36
Taurvi <srimbakusumo@gmail.com>
2015-09-09 21:22:37 -0700

    Initialized node.js files.

---
+++ node/index.js
@@ -0,0 +1,35 @@
+debugMode = true;
+
+debugMsg = function(msg) {
+    if (debugMode)
+        console.log('<<< DEBUG >>> ' + msg);
+}
+
+serverMsg = function(msg) {
+    console.log('|| SERVER || ' + msg);
+}
+
+serverMsg('node.js is initializing');
+
+// Initialization
+var app = require('express')();
+serverMsg(' Loaded express.');
+var http = require('http').Server(app);
+serverMsg(' Loaded http.');
+var mysql      = require('mysql');
+serverMsg(' Loaded mySql.');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'username',
+    password : 'pass',
+    database : 'database'
+});
+
+connection.connect();
+serverMsg(' Loaded mySql connection.');
+
+serverMsg('node.js is initialized.');
+
+http.listen(3000, function(){
+    serverMsg('Server is now listening on *:3000');
+});
